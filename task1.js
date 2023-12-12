7abbfaa71ba78aaf8def5452c5134e280ea8dc88
dev-rabin <robinmandhotia@gmail.com>
2023-12-12 10:53:22 +0530

    task-1-2-js

---
+++ task1.js
@@ -0,0 +1,7 @@
+let geographic_location = "Banglore";
+let job_title = "Full Stack Developer";
+
+document.write("You will be a "+ job_title+" "+geographic_location+"<br/>");
+// console.log(
+//     "You will be a "+ b+" "+a
+// );
---
+++ task2.js
@@ -0,0 +1,7 @@
+let first_name ="Ram"; let last_name = "Kumar"; let age = 24;
+
+console.log(
+    first_name,
+    last_name,
+    age
+);
